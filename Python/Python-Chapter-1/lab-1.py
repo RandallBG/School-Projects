6275ceb75204e8a450cbe12f4c15d7179f0c5f8d
RandallBG <Randall.bg.gosnell@gmail.com>
2021-10-27 01:36:09 -0400

    created lab-1

---
+++ Python/Python-Chapter-1/lab-1.py
@@ -0,0 +1,22 @@
+number = 0
+numberArr = []
+maximum = ''
+minimum = ''
+
+while number != -999:
+    number = int(input("Enter a number (-999 to exit)"))
+    if number != -999:
+        if minimum == '':
+            minimum = number
+        if maximum == '':
+            maximum = number
+        if number < minimum :
+            minimum = number
+        if number > maximum:g
+            maximum = number    
+        numberArr.append(number)
+
+print("Maximum", maximum, "\nMinimum", minimum, "Numbers entered: ", len(numberArr))
+
+    
+

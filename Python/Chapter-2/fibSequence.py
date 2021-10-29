753a7812abfe8e6014f905f151012bbf2f9a51d0
RandallBG <Randall.bg.gosnell@gmail.com>
2021-10-28 23:36:35 -0400

    added chapter for control statements and lab containing the problems and solutions

--- Python/Chapter-2/fibSequence.py
+++ Python/Chapter-2/fibSequence.py
@@ -1,15 +1,14 @@
 maxNum = int(input("Enter the amount of numbers in fib sequence"))
 numberArr = [0]
 
-
-
 for i in range(1,maxNum):
     if i > 1:
         numberArr.append(numberArr[i-1] + numberArr[i-2])
-        print(i, numberArr[i], numberArr[i-1], numberArr[i-2])
     if i == 1:
         numberArr.append(1)
 
 print(numberArr)
-
-print("Golden Ratio: 1.618034", "Your Ratio:", numberArr[maxNum-1]/ numberArr[maxNum-2])
+if(numberArr[maxNum-1] <=0 or numberArr[maxNum-2] <= 0):
+    print("you cant divide by 0 to find the golden ratio, enter a larger number next time")
+else:
+    print("Golden Ratio: 1.618034", "Your Ratio:", numberArr[maxNum-1]/ numberArr[maxNum-2])
---
+++ Python/Chapter-controlStatements/controlStatements.py
@@ -0,0 +1,44 @@
+# # Count by fives
+
+# # create function
+# def countByFive(maxNum):
+#     for i in range(0, maxNum+1, 5):
+#         print(i)
+
+
+# num = int(input("Enter a number to count to: "))
+# countByFive(num)
+
+
+# # determine if palindrome
+
+# def IsPalindrome(checkString):
+#     if(checkString == checkString[::-1]):
+#         print("This is a palindrome")
+#     else:
+#         print("This is not a palindrome")
+
+
+# palindrome = input("enter a word or number to check for palindrome: ")
+# IsPalindrome(palindrome)
+
+
+#Total up scores of multiple lists
+
+studentScores = [ "Snoop", [ 89, 78, 88,70, 95 ],  "Omar", [ 88, 88, 85,76, 89], "Stringer", [ 98, 93, 99, 95, 99 ], "Marlo", [ 79, 76, 88, 87, 100]  ]
+
+def studentTotals(scoreList):
+    total = 0
+    average = 0
+
+    for i in range(0 ,len(scoreList), 2):
+        total = 0
+        print(scoreList[i])
+        for x in range(0, len(scoreList[i+1])):
+            total += scoreList[i+1][x]
+        average = total/ len(scoreList[i+1])
+        print("total:", total)
+        print("average", average)
+
+
+studentTotals(studentScores)
